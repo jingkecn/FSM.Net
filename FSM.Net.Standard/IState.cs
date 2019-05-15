33729a7803e2c7d95dedf0bb38f10d3a13b020f1
Jing KE <jingkecn@gmail.com>
2019-05-15 15:35:15 +0200

    [add] state definitions.

---
+++ FSM.Net.Standard/IState.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace FSM.Net.Standard
+{
+    /// <summary>
+    ///     The interface for implementing a state in a state machine.
+    /// </summary>
+    public interface IState
+    {
+        /// <summary>
+        ///     <code>true</code> if a state is activated, otherwise <code>false</code>.
+        /// </summary>
+        bool IsActive { get; }
+
+        /// <summary>
+        ///     Called when a state is entered.
+        /// </summary>
+        void Enter();
+
+        /// <summary>
+        ///     Called when a command is to be executed by the state machine.
+        /// </summary>
+        /// <param name="command">Command to execute</param>
+        /// <returns><code>true</code> if execution has completed, otherwise <code>false</code></returns>
+        bool Execute(ICommand command);
+
+        /// <summary>
+        ///     Called when a state is exited.
+        /// </summary>
+        void Exit();
+    }
+}
